930186a3ad67a822314bc8853e497b0a70a8a700
JairoChoconta <jalexanderchoconta97@gmail.com>
2022-11-27 19:43:16 -0500

    Se agrega favicon y se terminan de crear nodos y traer la data de la API rick y morty

--- src/index.js
+++ src/index.js
@@ -3,4 +3,38 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+// url de mi API:
+const url = "https://rickandmortyapi.com/api/character";
+
+// para dejar todo en el div creado y no de último lugar del body:
+const appNode = document.querySelector('#app');
+
+// web API fetch (para traer recursos desde cualquier sitio web)
+// 1. Conerctarnos al servidor.
+window
+  .fetch(url)
+  // 2. Procesar la respuesta y convertirla en JSON.
+  .then((response) => response.json())
+  // 3. JSON -> Data -> Renderizar info en el browser.
+  .then((data) => {
+    const allItems = [];
+    data.results.forEach((item) => {
+      // crear imagen:
+      const imageCharacter = document.createElement("img");
+      imageCharacter.src = item.image; //url imagen
+      // crear título:
+      const title = document.createElement("h2");
+      title.textContent = item.name;
+      // crear especie:
+      const specieCharacter = document.createElement("div");
+      specieCharacter.textContent = item.species;
+      
+      //Cada grupo img, name, specie, se mete en un div:
+      const container = document.createElement("div"); 
+      container.append(imageCharacter, title, specieCharacter);
+
+      allItems.push(container);
+    });
+
+    appNode.append(...allItems);
+  });
