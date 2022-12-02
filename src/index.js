5404645acef73931b7150c9d376f923e87af7642
JairoChoconta <jalexanderchoconta97@gmail.com>
2022-12-01 19:19:55 -0500

    terminados y ajustados los estilos con TailwindCSS

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 const url = "https://rickandmortyapi.com/api/character";
 
 // para dejar todo en el div creado y no de último lugar del body:
-const appNode = document.querySelector('#app');
+const appNode = document.querySelector("#app");
 
 // web API fetch (para traer recursos desde cualquier sitio web)
 // 1. Conerctarnos al servidor.
@@ -22,16 +22,32 @@ window
       // crear imagen:
       const imageCharacter = document.createElement("img");
       imageCharacter.src = item.image; //url imagen
+      imageCharacter.className =
+        "h-48 w-48 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+
       // crear título:
       const title = document.createElement("h2");
       title.textContent = item.name;
+      title.className = "text-lg";
+
       // crear especie:
       const specieCharacter = document.createElement("div");
       specieCharacter.textContent = `Specie: ${item.species}`;
-      
-      //Cada grupo img, name, specie, se mete en un div:
-      const container = document.createElement("div"); 
-      container.append(imageCharacter, title, specieCharacter);
+      specieCharacter.className = "text-gray-600";
+
+      // Creación de un contenedor para título y especie:
+      const infoCharacter = document.createElement("div");
+      infoCharacter.append(title, specieCharacter);
+      infoCharacter.className = "text-center md:text-left";
+
+      // Por último, creación de una carta con toda la info del caracter:
+      const card = document.createElement("div");
+      card.append(imageCharacter, infoCharacter);
+      card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";      
+
+      //Cada grupo el contenedor de info e imagen se meten en un contenedor principal:
+      const container = document.createElement("div");
+      container.append(card);
 
       allItems.push(container);
     });
